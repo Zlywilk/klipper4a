15edeb682783907453bc5d0f44e9f1d2f36551bc
Wiktor Wołk <wiktor.wolk91@gmail.com>
2022-08-31 08:37:37 +0200

    fix typo

--- janus.sh
+++ janus.sh
@@ -6,7 +6,7 @@ export LIBWEBSOCKETS_VERSION="v3.2.2"
 export USRSCTP_VERSION="0.9.3.0"
 export JANUS_VERSION="v0.10.3"
 if [ "$DISTRO" == "alpine" ]; then
-	sudo apk update && sudo apk add libmicrohttpd jansson-dev sofia-sip-dev glib-dev opus-dev libogg-dev libconfig-dev pkgconfig gengetopt 	libsrtp-dev libtool automake libconfig-dev gtk-doc cmake autoconf m4 ninja meson libmicrohttpd-dev jansson-dev opus-dev curl-dev ibpq-dev libc-dev musl-dev build-base doxygen graphviz gstreamer-dev 	autoconf graphviz doxygen guile
+	sudo apk update && sudo apk add libmicrohttpd jansson-dev sofia-sip-dev glib-dev opus-dev libogg-dev libconfig-dev pkgconfig gengetopt 	libsrtp-dev libtool automake libconfig-dev gtk-doc cmake autoconf m4 ninja meson libmicrohttpd-dev jansson-dev opus-dev curl-dev libpq-dev libc-dev musl-dev build-base doxygen graphviz gstreamer-dev 	autoconf graphviz doxygen guile
 	mkdir ~/janus && cd ~/janus || exit 
 	wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz && tar xfv v2.2.0.tar.gz && cd libsrtp-2.2.0 && ./configure --enable-openssl && make -j"${nproc}" shared_library && sudo make install
 cd ~/janus && wget https://github.com/warmcat/libwebsockets/archive/refs/tags/v2.4.2.tar.gz && tar zxvf v2.4.2.tar.gz && cd libwebsockets-2.4.2/ && mkdir build && cd build && cmake .. -DLWS_WITH_SSL=OFF -DLWS_WITHOUT_BUILTIN_SHA1=OFF && make -j"${nproc}" && sudo make install
